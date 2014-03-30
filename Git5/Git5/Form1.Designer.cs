5f136acfa4cb66fa2720b7f55bc711474e940841
Kamil <kamilfarnicki@outlook.com>
2014-03-30 16:03:53 +0200

    TOM

---
+++ Git5/Git5/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Git5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.renderWindowControl1 = new Kitware.VTK.RenderWindowControl();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // renderWindowControl1
+            // 
+            this.renderWindowControl1.AddTestActors = false;
+            this.renderWindowControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.renderWindowControl1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.renderWindowControl1.Location = new System.Drawing.Point(12, 12);
+            this.renderWindowControl1.Name = "renderWindowControl1";
+            this.renderWindowControl1.Size = new System.Drawing.Size(402, 344);
+            this.renderWindowControl1.TabIndex = 0;
+            this.renderWindowControl1.TestText = null;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoad.Location = new System.Drawing.Point(12, 362);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 1;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSave.Location = new System.Drawing.Point(94, 362);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 397);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.renderWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Kitware.VTK.RenderWindowControl renderWindowControl1;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ Git5/Git5/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Kitware.VTK;
+
+// Labo 5: wczytanie i zapis plików (butelka i stopa)
+
+namespace Git5
+{
+    public partial class Form1 : Form
+    {
+        private vtkActor stlActor;
+        private vtkActor coneActor;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)                   // wczytywanie pliku
+        {
+            vtkSTLReader stlReader = vtkSTLReader.New();                            // źródłem będzie plik .stl więc zostaje nam tylko mapper, renderer i aktor
+            stlReader.SetFileName("C:/bottle.stl");                                 // ścieżkę można zapisać na 2 sposoby, to pierwszy sposób z użyciem normalnych ukośników "/"
+
+            vtkPolyDataMapper stlMapper = vtkPolyDataMapper.New();                  
+            stlMapper.SetInputConnection(stlReader.GetOutputPort());
+
+            vtkActor stlActor = vtkActor.New();
+            stlActor.SetMapper(stlMapper);
+
+            vtkRenderWindow renderWindow = renderWindowControl1.RenderWindow;
+            vtkRenderer renderer = renderWindow.GetRenderers().GetFirstRenderer();
+            renderer.AddActor(stlActor);                                            //aktor powinien się pojawić, naciśnij "r" aby wycentrować i "w" aby wyświetlić siatkę
+
+            this.renderWindowControl1.Refresh();
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)               // wyświetlenie stożka i jego zapis
+        {
+            vtkConeSource coneSource = vtkConeSource.New();
+            coneSource.SetResolution(30);
+
+            vtkPolyDataNormals phongFilter = vtkPolyDataNormals.New();          // jeżeli chcemy olać wyświetlanie stożka i od razu go zapisać można zakomentować od tej linii
+            phongFilter.SetInputConnection(coneSource.GetOutputPort());             // tutaj jest filtr do stożka
+
+            vtkPolyDataMapper coneMapper = vtkPolyDataMapper.New();                 
+            coneMapper.SetInputConnection(phongFilter.GetOutputPort());             //mapper musi wskazywać na filtr
+
+            this.coneActor = vtkActor.New();
+            this.coneActor.SetMapper(coneMapper);
+
+            vtkRenderWindow renWin = this.renderWindowControl1.RenderWindow;
+            vtkRenderer renderer = renWin.GetRenderers().GetFirstRenderer();
+            renderer.AddActor(coneActor);
+
+            this.renderWindowControl1.Refresh();                                // do tej. Stożek zostanie zapisany bez wyświetlania 
+
+            vtkSTLWriter stlWriter = vtkSTLWriter.New();
+            stlWriter.SetInputConnection(coneSource.GetOutputPort());
+            stlWriter.SetFileName("C:\\Users\\Killy\\stozek.stl");              // 2 sposób zapisywania (lewe ukośniki \), nie jest możliwe proste skopiowanie i wklejenie ścieżki dostępu
+            stlWriter.SetFileTypeToASCII();                                     // zapisujemy to jako zwykły plik.
+            stlWriter.Write();
+        }
+    }
+}
